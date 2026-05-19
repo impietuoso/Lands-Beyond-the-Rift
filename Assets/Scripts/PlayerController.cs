70b93754464c6e914e54c486ff365d306f601ee7
Impietuoso <palidoman@gmail.com>
2026-05-19 18:33:51 -0300

    New Base Scene, New Player Prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed;
+    public Vector2 axis { get; private set; }
+    public SpriteRenderer spr;
+    public Rigidbody2D rb;
+    public Animator anim;
+    private bool facingRight = true;
+
+    public void Update() {
+        if(rb) rb.linearVelocity = axis.normalized * moveSpeed;
+        if(anim) anim.SetBool("Moving",axis.sqrMagnitude != 0);
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        axis = context.ReadValue<Vector2>();
+        if(axis.x > 0 && !facingRight) Flip();
+        if(axis.x < 0 && facingRight) Flip();
+    }
+
+    public void Interact(InputAction.CallbackContext context) {
+        Debug.Log("Interagiu!");
+    }
+    
+    public void Flip() {
+        if(spr) spr.flipX = !spr.flipX;
+        facingRight = !facingRight;
+    }
+}
