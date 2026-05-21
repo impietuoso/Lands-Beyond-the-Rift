e36a30a48f80b90618869142930a16701904072c
Impietuoso <palidoman@gmail.com>
2026-05-21 01:09:40 -0300

    Scene Transition, New Sprites for Sound Skills

---
+++ Assets/Scripts/Controllers/LoadController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadController : MonoBehaviour {
+    public Animator loadscreen;
+    public float loadDelay;
+    public void ChangeScene(int sceneId) {
+        Fade();
+        StartCoroutine(Loading(sceneId));
+    }
+
+    public IEnumerator Loading(int sceneId) {
+        yield return new WaitForSeconds(loadDelay);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        operation.completed += Fade;
+    }
+
+    private void Fade(AsyncOperation obj) {
+        Fade();
+    }
+
+    public void Fade() {
+        loadscreen.SetTrigger("Fade");
+    }
+}
