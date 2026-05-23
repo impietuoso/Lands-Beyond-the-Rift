263336f0f664d8e08d23a9927112fa4c7ff386df
Iago Fonteles <iagofonteles1@gmail.com>
2026-05-23 20:39:32 -0300

    clone

--- Assets/TurnBasedRPG/UnityExtensions.cs
+++ Assets/TurnBasedRPG/UnityExtensions.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace TurnBasedRPG {
     public static class UnityExtensions {
+        public static T Clone<T>(this T obj, Transform parent = null) where T : Object => Object.Instantiate(obj, parent);
         public static T Clone<T>(this T obj, Vector3 pos) where T : Object => Object.Instantiate(obj, pos, Quaternion.identity);
     }
 }
